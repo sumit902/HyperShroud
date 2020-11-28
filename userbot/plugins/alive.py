88848985c5395f8c0c085cdf1a1fd1e5a2b438d2
NotShroudX97 <67467393+NotShroudX97@users.noreply.github.com>
2020-11-28 11:59:40 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -14,22 +14,27 @@ else:
 
 kraken = bot.uid
 
-PM_IMG = "https://telegra.ph/file/80e5200c615cf0cb57aa9.mp4"
-pm_caption = "__**🔥🔥ɦɛʟʟɮօt ɨs օռʟɨռɛ🔥🔥**__\n\n"
+PM_IMG = "https://telegra.ph/file/64f0450a2f15dfdb14b43.png"
+pm_caption = "âž¥ **HyperShroud/PeruUserBot Is:** `Active`\n\n"
+pm_caption += "âž¥ **SYSTEMS STATUS**\n"
+pm_caption += f"☣HyperShroud☣ : `{hellversion}`\n"
+pm_caption += "âž¥ **Telethon Version:** `1.15.0` \n"
+pm_caption += "âž¥ **Python:** `3.9.0` \n"
+pm_caption += f"âž¥ **Uptime** : `{uptime}` \n"
+pm_caption += "âž¥ **Database Status:**  `Functional`\n"
+pm_caption += "âž¥ **Current Branch** : `Master`\n"
+pm_caption += f"âž¥ **Version** : `{currentversion}`\n"
+pm_caption += f"âž¥ **My Peru Master** : {@NotShroudX97} \n"
+pm_caption += "âž¥ **My Peru Assistant** : @HyperShroudXBot
+pm_caption += "âž¥ **Heroku Database** : `AWS - Working Properly Noice`\n\n"
+pm_caption += "âž¥ **License** : [GNU General Public License v3.0](github.com/NotShroudX97/HyperShroud/blob/master/LICENSE)\n"
+pm_caption += "âž¥ **Copyright** : By [NotShroudX97@Github](GitHub.com/NotShroudX97)\n"
+pm_caption += "âž¥ **Check Stats By Doing** `.stat`. \n\n"
+pm_caption += "[Join My Gang Here @HyperMTKGang](t.me/HyperMTKGang)"
 
-pm_caption += f"               __↼🄼🄰🅂🅃🄴🅁⇀__\n**『[{DEFAULTUSER}](tg://user?id={kraken})』**\n\n"
+pm_caption += "☣The Creator God☣ : [Master Peru Saar Here](https://t.me/NotShroudX97)\n\n"
 
-pm_caption += "🛡️TELETHON🛡️ : `1.15.0` \n"
-
-pm_caption += f"😈Hêllẞø†😈       : `{hellversion}`\n"
-
-pm_caption += f"⚜️Sudo⚜️            : `{sudou}`\n"
-
-pm_caption += "⚠️CHANNEL⚠️   : [ᴊᴏɪɴ](https://t.me/HellBot_Official)\n"
-
-pm_caption += "🔥CREATOR🔥    : [Nub Here](https://t.me/kraken_the_badass)\n\n"
-
-pm_caption += "      [✨REPO✨](https://github.com/hellboy-op/hellbot) 🔹 [📜License📜](https://github.com/HellBoy-OP/HellBot/blob/master/LICENSE)"
+pm_caption += "[☣Repository☣](https://github.com/NotShroudX97/HyperShroud) [☣License☣](https://github.com/NotShroudX97/HyperShroud/blob/master/LICENSE)" [📜License📜](https://github.com/HellBoy-OP/HellBot/blob/master/LICENSE)"
 
 @bot.on(admin_cmd(outgoing=True, pattern="alive$"))
 @bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
